06e030da7720f9f5ca411840e3f03c8b98c2d222
Nicele Reyes <nicele.reyes26@gmail.com>
2024-11-01 15:48:21 +0800

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import ErrorPage from "./ErrorPage";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("716300");
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="*" element={<ErrorPage />} />
-        {/* Fallback for unknown routes */}
-      </Routes>
-    </Router>
+    <div className="App">
+      <Header foodData={foodData} setFoodData={setFoodData} />
+      <Recipes foodData={foodData} setFoodId={setFoodId} foodId={foodId} />
+    </div>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,1 @@
-import Header from "./Header";
-import Recipes from "./Recipes";
-export default function Home() {
-  const [foodData, setFoodData] = useState([]);
-  const [foodId, setFoodId] = useState("716300");
-  return (
-    <div className="App">
-      <Header foodData={foodData} setFoodData={setFoodData} />
-      <Recipes foodData={foodData} setFoodId={setFoodId} foodId={foodId} />
-    </div>
-  );
-}
+export default function Home() {}
