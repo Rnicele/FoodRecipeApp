acadd60b2f81942c7660071be86a2d65de4aaa81
Nicele Reyes <nicele.reyes26@gmail.com>
2024-11-01 15:40:48 +0800

    add error bopundary

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import ErrorPage from "./ErrorPage";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="*" element={<ErrorPage />} />{" "}
-        {/* Fallback for unknown routes */}
-      </Routes>
-    </Router>
+    <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<ErrorPage />} />
+          {/* Fallback for unknown routes */}
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
