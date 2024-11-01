5a432ee4d821ea27d06a941d8c40f4ac115dba4c
Nicele Reyes <nicele.reyes26@gmail.com>
2024-11-01 14:57:10 +0800

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ import "./App.css";
 import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
-  const [foodData, setFoodData] = useState([]);
-  const [foodId, setFoodId] = useState("716300");
   return (
     <Router>
       <ErrorBoundary>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Define other routes as needed */}
-            <Route path="*" element={<ErrorPage />} />
-            {/* Fallback for unknown routes */}
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Define other routes as needed */}
+          <Route path="*" element={<ErrorPage />} />
+          {/* Fallback for unknown routes */}
+        </Routes>
       </ErrorBoundary>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,8 @@ import styles from "../styles/home.module.css";
 import Header from "./components/Header";
 import Recipes from "./components/Recipes";
 export default function Home() {
-  throw new Error("Test error to trigger ErrorBoundary");
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("716300");
   return (
     <div>
       <Header foodData={foodData} setFoodData={setFoodData} />
